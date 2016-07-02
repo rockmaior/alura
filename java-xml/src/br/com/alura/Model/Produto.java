21409060ab39c31d459993c1f844456e0c8180f7
Reginaldo <rockmaior@gmail.com>
2016-07-02 13:59:02 -0300

    Equalizando

--- java-xml/src/br/com/alura/Model/Produto.java
+++ java-xml/src/br/com/alura/Model/Produto.java
@@ -35,3 +35,40 @@ public class Produto {
 	}
 
 }
+package br.com.alura.Model;
+
+public class Produto {
+	private String nome;
+	private double preco;
+
+//	public Produto(String nome, double preco) {
+//		this.nome = nome;
+//		this.preco = preco;
+//	}
+//
+//	public Produto() {
+//	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Nome: " + nome + "\nPreco: " + preco + "\n";
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+}
