8d570a573da4ed68891611283c9f5471460f4f89
Reginaldo <rockmaior@gmail.com>
2016-07-02 11:15:48 -0300

    Revert "Comitando até aula 05"
    This reverts commit 064ba9dcb2c7d540aea39c9ed6fc128dcd34d1aa.

--- jms/src/br/com/caelum/jms/TesteConsumidor.java
+++ jms/src/br/com/caelum/jms/TesteConsumidor.java
@@ -1,38 +1,52 @@
 package br.com.caelum.jms;
 
-import java.util.Enumeration;
+import java.util.Scanner;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
-import javax.jms.Queue;
-import javax.jms.QueueBrowser;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
-public class TesteQueueBrowser {
-
+public class TesteConsumidor {
 	public static void main(String[] args) throws Exception {
+
 		InitialContext context = new InitialContext();
+
 		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
 		Connection connection = factory.createConnection();
 		connection.start();
 
+		// Cria o consumidor
+
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 		Destination fila = (Destination) context.lookup("financeiro");
-		QueueBrowser browser = session.createBrowser((Queue) fila);
+		MessageConsumer consumer = session.createConsumer(fila);
 
-		Enumeration msgs = browser.getEnumeration();
-		while (msgs.hasMoreElements()) {
-			TextMessage msg = (TextMessage) msgs.nextElement();
-			System.out.println("Message: " + msg.getText());
-		}
+		consumer.setMessageListener(new MessageListener() {
+
+			@Override
+			public void onMessage(Message message) {
+				TextMessage textMessage = (TextMessage) message;
+				try {
+					System.out.println(textMessage.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+		new Scanner(System.in).nextLine();
 
 		session.close();
 
 		connection.close();
 		context.close();
 	}
-
 }
